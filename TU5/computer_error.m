a78c2091229fb98f48c5f4d16717a38f0354f511
Marcus TM <146819299+marcusteme@users.noreply.github.com>
2025-10-29 09:39:25 +0100

    Added Calculation to search for the stepsize where Error is < 0.05, but i dont think it works properly.
    In addition, the second solver only works, when t_final = 10 (cant use different final times)

--- TU5/computer_error.m
+++ TU5/computer_error.m
@@ -1,12 +1,12 @@
 function [abs_error, mean_abs_error, rel_error, mean_rel_error] = computer_error(y_exact, y_num)
     % Absolute error
-    abs_error = y_exact - y_num;
+    abs_error = abs(y_exact - y_num);
 
     % Average absolute error
     mean_abs_error = mean(abs_error);
 
     % Relative error in percentage
-    rel_error = 100 * (1 - y_num ./ y_exact);
+    rel_error = 100 * abs_error ./ max(abs(y_exact), eps);
 
     % Average relative error
     mean_rel_error = mean(rel_error);
