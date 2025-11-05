d2885be90e7f7f10957df208396e80cc6ac503c6
Marcus TM <146819299+marcusteme@users.noreply.github.com>
2025-11-05 13:39:17 +0100

    Matlab Function to find nth Fibo Number

---
+++ TU6/Fibonacci.m
@@ -0,0 +1,15 @@
+function nthFiboNum = Fibonacci(n)
+    arguments
+        n (1,1) {mustBeNumeric, mustBeNonnegative, mustBeInteger}
+    end
+
+    if n == 0
+        nthFiboNum = 0;
+        return;
+    elseif n == 1
+        nthFiboNum = 1;
+        return;
+    end
+
+    nthFiboNum = Fibonacci(n-1) + Fibonacci(n-2);
+end
